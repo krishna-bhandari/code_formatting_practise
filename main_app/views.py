b47b042979da801caa440b08e0bbe297ba05adb0
Krishna Bhandari <krishnabhandarigit@outlook.com>
2022-06-17 12:31:49 +0545

    save

---
+++ main_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from django.contrib.auth.models import get_auth_model
+# Create your views here.
+User = get_auth_model()
+
+
+class UserListView():
+    pass
+class UserDetailView():
+    
+    pass
